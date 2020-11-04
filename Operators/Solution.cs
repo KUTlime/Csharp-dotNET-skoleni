8ce7ef07bd53ccbed925e64d7edb25aa3255b4c4
Student CZ <Student CZ@PC100>
2020-11-04 16:08:17 +0100

    Add operator chapter

---
+++ Operators/Solution.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Operators
+{
+	class Solution
+    {
+        static UInt64 GenerateMultipleOfTwo(UInt16 n)
+        {
+            const UInt64 two = 2;
+            return n == 0 ? 1 : two << (n - 1);
+        }
+        /*
+        static void Main()
+        {
+            Console.WriteLine(GenerateMultipleOfTwo(2));
+        }
+        */
+    }
+}
+
+
---
+++ Operators/Task.cs
@@ -0,0 +1,10 @@
+﻿namespace Operators
+{
+	// Navrhněte metodu, která bude vracet UInt64 a jako vstupní parametr
+	// bude přebírat n-mocninu 2ky (1. mocnina = 2, 2. mocnina = 4 atd).
+	// N-násobek můžeme přečíst jako UInt16 = Console.ReadLine()
+	// Popřemýšlejte, co mají násobky 2 společné.
+	class Task
+	{
+	}
+}
