9822e669dfc6d078b102a4ae387334b268039ef7
KUTlime <kutlime@gmail.com>
2020-11-05 06:55:19 +0100

    Add task and solution to Exceptions

---
+++ Exceptions/Task.cs
@@ -0,0 +1,12 @@
+﻿namespace Exceptions
+{
+	class Task
+	{
+		/*
+     * 1. Nadefinujte si vlastní výjimku. Vytvořte v ní tři konstruktory dle rady výše.
+     * 2. Napište metodu, která bude výjimku vyhazovat.
+     * 3. Zkuste výjimku podle buď hodnoty parametru uvnitř výjimky nebo např. podle
+     *    toho, jestli už máte po pracovní době či nikoliv.
+     */
+	}
+}
