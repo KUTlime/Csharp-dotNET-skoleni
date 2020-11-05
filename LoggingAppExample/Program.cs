88eca44398cd7d3170d8fa4573177a28f89d9550
KUTlime <kutlime@gmail.com>
2020-11-05 06:54:27 +0100

    Add logging example

---
+++ LoggingAppExample/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Utils;
+
+namespace LoggingAppExample
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			ILog dynamicLogger = new ConsoleLogger();
+			string logType = args[0];
+			switch (logType)
+			{
+				case "/t":
+					dynamicLogger = new FileLogger();
+					break;
+				case "/c":
+					dynamicLogger = new ConsoleLogger();
+					break;
+				default:
+					dynamicLogger = new Log();
+					break;
+			}
+
+			Log.WriteLine($"Hello world");
+			Log.Write("Hello world");
+
+			ILog logger = Log.LoggerStatic;
+
+			logger.WriteLine("asdf");
+
+
+			Console.ReadKey();
+		}
+	}
+
+}
---
+++ Utils/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Utils
+{
+	public interface ILog
+	{
+		void WriteLine(string message);
+		void Write(string message);
+	}
+
+	public class Log : ILog
+	{
+		public ILog Logger { get; set; } = new ConsoleLogger();
+		public static ILog LoggerStatic { get; set; } = new ConsoleLogger();
+
+
+		public static void WriteLine(string message)
+		{
+		}
+
+		void ILog.Write(string message)
+		{
+			Logger.Write(message);
+		}
+
+		void ILog.WriteLine(string message)
+		{
+			Logger.WriteLine(message);
+		}
+
+		public static void Write(string message)
+		{
+			Console.Write($"[{DateTime.UtcNow}]: {message}");
+		}
+	}
+
+	public class ConsoleLogger : ILog
+	{
+		public void WriteLine(string message)
+		{
+			Console.WriteLine($"[{DateTime.UtcNow}]: {message}");
+		}
+
+		public void Write(string message)
+		{
+			Console.Write($"[{DateTime.UtcNow}]: {message}");
+		}
+	}
+
+	public class FileLogger : ILog
+	{
+		public void WriteLine(string message)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Write(string message)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
