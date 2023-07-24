cff1f572c8af9b63db259e8298067261329fc088
Radek Zahradník <kutlime@gmail.com>
2023-07-24 13:18:21 +0200

    Add examples about fizz buzz

---
+++ TestableFizzBuzz.Tests/FizzBuzzTests.cs
@@ -0,0 +1,37 @@
+﻿namespace TestableFizzBuzz.Tests;
+
+public static class FizzBuzzTests
+{
+    public class FunctionalImplementationTests
+    {
+        [Fact]
+        public void BasicTest()
+        {
+            // AAA
+            // Arrange
+            // Act
+            // Assert
+            var testvalues = new FizzBuzz()
+                .FunctionalImplementation()
+                .ToList();
+
+            testvalues
+                .Should()
+                .HaveCount(100)
+                .And
+                .HaveElementAt(0, "1")
+                .And
+                .HaveElementAt(1, "2")
+                .And
+                .HaveElementAt(2, "Fizz")
+                .And
+                .HaveElementAt(4, "Buzz")
+                .And
+                .HaveElementAt(14, "FizzBuzz")
+                .And
+                .HaveElementAt(41, "Fizz")
+                .And
+                .HaveElementAt(42, "43");
+        }
+    }
+}
---
+++ TestableFizzBuzz/FizzBuzz.cs
@@ -0,0 +1,142 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Engines;
+using Benchmarks;
+
+namespace TestableFizzBuzz;
+
+[Config(typeof(AntiVirusFriendlyConfig))]
+[MemoryDiagnoser]
+public class FizzBuzz
+{
+    private readonly Consumer consumer = new Consumer();
+
+    public IEnumerable<string> FunctionalImplementation()
+        => Enumerable
+            .Range(1, 100)
+            .Select(i => i switch
+            {
+                _ when i % 5 == 0 && i % 3 == 0 => "FizzBuzz",
+                _ when i % 3 == 0 => "Fizz",
+                _ when i % 5 == 0 => "Buzz",
+                _ => i.ToString()
+            });
+
+    [Benchmark(Baseline = true)]
+    public void ConsumeFunctionalImplementation()
+        => FunctionalImplementation().Consume(consumer);
+
+    public IEnumerable<string> ListForImplementation()
+    {
+        var list = new List<string>();
+        for (var i = 1;  i <= 100; i++)
+        {
+            if(i % 3 == 0 && i % 5 == 0)
+            {
+                list.Add("FizzBuzz");
+                continue;
+            }
+            if (i % 3 == 0)
+            {
+                list.Add("Fizz");
+                continue;
+            }
+            if (i % 5 == 0)
+            {
+                list.Add("Buzz");
+                continue;
+            }
+            list.Add(i.ToString());
+        }
+        return list;
+    }
+
+    [Benchmark]
+    public void ConsumeListForImplementation()
+        => ListForImplementation().Consume(consumer);
+
+
+    public IEnumerable<string> ArrayForImplementation()
+    {
+        var array = new string[100];
+        for (var i = 1; i <= 100; i++)
+        {
+            if (i % 3 == 0 && i % 5 == 0)
+            {
+                array[i-1] = "FizzBuzz";
+                continue;
+            }
+            if (i % 3 == 0)
+            {
+                array[i-1] = "Fizz";
+                continue;
+            }
+            if (i % 5 == 0)
+            {
+                array[i-1] = "Buzz";
+                continue;
+            }
+            array[i-1] = i.ToString();
+        }
+        return array;
+    }
+
+    [Benchmark]
+    public void ConsumeArrayForImplementation()
+        => ArrayForImplementation().Consume(consumer);
+
+    public IEnumerable<string> ListWith100PlacesForImplementation()
+    {
+        var list = new List<string>(100);
+        for (var i = 1; i <= 100; i++)
+        {
+            if (i % 3 == 0 && i % 5 == 0)
+            {
+                list.Add("FizzBuzz");
+                continue;
+            }
+            if (i % 3 == 0)
+            {
+                list.Add("Fizz");
+                continue;
+            }
+            if (i % 5 == 0)
+            {
+                list.Add("Buzz");
+                continue;
+            }
+            list.Add(i.ToString());
+        }
+        return list;
+    }
+
+    [Benchmark]
+    public void ConsumeListWith100PlacesForImplementation()
+        => ListWith100PlacesForImplementation().Consume(consumer);
+
+    public IEnumerable<string> YieldForImplementation()
+    {
+        for (var i = 1; i <= 100; i++)
+        {
+            if (i % 3 == 0 && i % 5 == 0)
+            {
+                yield return "FizzBuzz";
+                continue;
+            }
+            if (i % 3 == 0)
+            {
+                yield return "Fizz";
+                continue;
+            }
+            if (i % 5 == 0)
+            {
+                yield return "Buzz";
+                continue;
+            }
+            yield return i.ToString();
+        }
+    }
+
+    [Benchmark]
+    public void ConsumeYieldForImplementation()
+        => YieldForImplementation().Consume(consumer);
+}
